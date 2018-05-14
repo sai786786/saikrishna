3ae99b3cf3834926c9491be82e491684b84cb977
sai786786 <33508924+sai786786@users.noreply.github.com>
2018-05-14 17:25:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var config = require('./config.js').get(process.env.NODE_ENV);
 
 var server = http.createServer(function (req, res) {
     res.writeHead(200);
-    res.end('Hi everybody! ' + config.common.ServerName + ' follow us on :' + config.twitter.callbackURL + 'it's working:');
+    res.end('Hi everybody! ' + config.common.ServerName + ' follow us on :' + config.twitter.callbackURL +' ok');
 });
 server.listen(config.common.port, () => console.log('Started'));
